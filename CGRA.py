73d4ff5c9d3be8dd98a4aa00779aae94f019b057
lkds <2576027877@qq.com>
2020-05-09 11:32:45 +0800

    version-with-bug

--- CGRA.py
+++ CGRA.py
@@ -1,8 +1,43 @@
-from pulp import LpVariable
+from pulp import LpMinimize, LpProblem, LpVariable, lpSum
+import pandas
 
-x = LpVariable("x", lowBound=None, upBound=None, cat='Integer')
-y = LpVariable("y", lowBound=None, upBound=None, cat='Integer')
-prob = LpProblem("myProblem", LpMinimize)
-prob += x + y <= 2
-prob += -4*x + y
+# 读取数据
+df = pandas.read_csv("src/g1.CSV", header=None)
+# 获取行数和列数
+maxNode = df.shape[0]
+maxStep = df.shape[1]
+#
+ii = 3
+# 构造变量字典
+vName = [[_ for _ in range(maxStep)] for _ in range(maxNode)]
+
+# 创建问题实例
+prob = LpProblem("CGRA_Problem", LpMinimize)
+
+# 创建变量字典
+vDict = []
+for i in range(maxNode):
+    vDict.append(LpVariable.dicts('V', vName[i], 0, 1, 'Integer'))
+
+maxPE = LpVariable("max_PE_node", 0, 16, cat='Integer')
+# 添加目标方程
+prob += maxPE
+
+# 添加约束
+# 最大最小约束
+for i in range(maxStep):
+    prob += lpSum(vDict[:][i]) - lpSum(maxPE) < 0
+# 资源约束
+for i in range(maxStep):
+    prob += lpSum(vDict[:][i]) <= 16
+# 长/依赖约束
+for i in range(maxNode):
+    for k in range(1, 5):
+        if(df[i][k] != 0):
+            prob += lpSum(vDict[i][j] * j for j in range(maxStep)
+                          ) < lpSum(vDict[df[i][k] - 1][m] * m for m in range(maxStep))
+            prob += lpSum(vDict[i][j] * j for j in range(maxStep)
+                          ) - lpSum(vDict[df[i][k] - 1][m] * m for m in range(maxStep)) % ii != 0
+
 status = prob.solve()
+print(status)
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CGRA_Software_Pipeline_Schedule
 Coarse-Grained Reconfigurable Architecture supported software pipeline loop scheduling project
+
+## 背景描述
+### CGFA：粗粒度可重构架构
+### 软件流水：最小化循环间隔
+
+## 问题定义
+### 目标
+对循环体 DFG 进行调度，使调度之后的DFG 满足资源约束和长依赖约束，最小化CGRA计算阵列的处理单元使用量
+### 约束
+* **依赖约束**
+  * $$t_j\$$
+* **资源约束**
+* **依赖长度约束**
