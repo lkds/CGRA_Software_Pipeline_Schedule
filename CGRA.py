58579d7df4b57a8545c9900618b32430973c0577
lkds <2576027877@qq.com>
2020-05-09 15:04:22 +0800

    v_bug_2

--- CGRA.py
+++ CGRA.py
@@ -1,15 +1,16 @@
-from pulp import LpMinimize, LpProblem, LpVariable, lpSum
+from pulp import LpMinimize, LpProblem, LpVariable, lpSum, LpMaximize, LpSolverDefault
 import pandas
 
 # 读取数据
 df = pandas.read_csv("src/g1.CSV", header=None)
 # 获取行数和列数
 maxNode = df.shape[0]
-maxStep = df.shape[1]
-#
+maxStep = maxNode
 ii = 3
+
+df = df.T
 # 构造变量字典
-vName = [[_ for _ in range(maxStep)] for _ in range(maxNode)]
+vName = [[i+j*maxStep for i in range(maxStep)] for j in range(maxNode)]
 
 # 创建问题实例
 prob = LpProblem("CGRA_Problem", LpMinimize)
@@ -20,13 +21,15 @@ for i in range(maxNode):
     vDict.append(LpVariable.dicts('V', vName[i], 0, 1, 'Integer'))
 
 maxPE = LpVariable("max_PE_node", 0, 16, cat='Integer')
+# kp = LpVariable('kp', 1, 100, cat='Integer')
 # 添加目标方程
 prob += maxPE
+# prob += lpSum(vDict[:][i])
 
 # 添加约束
 # 最大最小约束
 for i in range(maxStep):
-    prob += lpSum(vDict[:][i]) - lpSum(maxPE) < 0
+    prob += lpSum(vDict[:][i]) <= lpSum(maxPE)
 # 资源约束
 for i in range(maxStep):
     prob += lpSum(vDict[:][i]) <= 16
@@ -34,10 +37,11 @@ for i in range(maxStep):
 for i in range(maxNode):
     for k in range(1, 5):
         if(df[i][k] != 0):
-            prob += lpSum(vDict[i][j] * j for j in range(maxStep)
-                          ) < lpSum(vDict[df[i][k] - 1][m] * m for m in range(maxStep))
-            prob += lpSum(vDict[i][j] * j for j in range(maxStep)
-                          ) - lpSum(vDict[df[i][k] - 1][m] * m for m in range(maxStep)) % ii != 0
+            prob += lpSum(vDict[i][j+maxStep*i] * j for j in range(maxStep)
+                          ) <= lpSum(vDict[df[i][k] - 1][m+maxStep*(df[i][k] - 1)] * m for m in range(maxStep))
+            # prob += lpSum(vDict[i][j+maxStep*i] * j for j in range(maxStep)
+            #               ) == lpSum(vDict[df[i][k] - 1][m+maxStep*(df[i][k] - 1)] * m for m in range(maxStep))
 
+LpSolverDefault.msg = 1
 status = prob.solve()
 print(status)
